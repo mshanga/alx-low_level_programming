4103564d72e4a5950e4dbed15823c76fad25cdc6
“Emmanuel <emakonde@gmail.com>
2022-09-19 11:30:54 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,17 @@
-include "main.h"
+#include "main.h"
+
 /**
-* _strlen - returns the length of a string.
-* @s: input string.
-* Return: length of a string.
+* _strlen - returns the length of a string
+* @s: string
+* Return: returns length as integer;
 */
+
 int _strlen(char *s)
 {
-int count = 0;
-while (*(s + count) != '\0')
-count++;
-return (count);
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
 }
