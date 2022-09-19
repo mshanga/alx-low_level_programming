7b81fda57f4541d068aed2730be8a9dea4bd929a
“Emmanuel <emakonde@gmail.com>
2022-09-19 11:34:05 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,23 +1,22 @@
-include "main.h"
+#include <stdio.h>
+#include "main.h"
+
 /**
- * _puts - prints a string, followed by a new line.
- * @str: input string.
- * Return: no return.
- */
+*_puts - prints a string
+* @str: string to print
+*
+* Description: prints a string
+* On success: returns no error
+*/
+
 void _puts(char *str)
 {
-int count = 0;
+	int i = 0;
 
-while (count >= 0)
-{
-if (str[count] == '\0')
-{
-_putchar('\n');
-break;
+	while (*(str + i) != '\0')
+	{
+		putchar(*(str + i));
+		i++;
+	}
+	putchar(10);
 }
-_putchar(str[count]);
-count++;
-}
-}
-
-
