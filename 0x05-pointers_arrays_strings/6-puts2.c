789fa8fa35bc7b3b56edab68a33c4dbba7dd0cd8
“Emmanuel <emakonde@gmail.com>
2022-09-19 11:46:12 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,22 +1,22 @@
-include "main.h"
+#include <stdio.h>
+#include "main.h"
+
 /**
-* puts2 - prints one char out of 2 of a string.
-* @str: input string.
-* Return: no return.
+* puts2 - prints every other character
+* @str: string
+*
+* Return: nothing
 */
+
 void puts2(char *str)
 {
-int count = 0;
+	int i = 0;
 
-while (count >= 0)
-{
-if (str[count] == '\0')
-{
-_putchar('\n');
-break;
-}
-if (count % 2 == 0)
-_putchar(str[count]);
-count++;
-}
+	while (*(str + i) != '\0')
+	{
+		if (i % 2 == 0)
+			putchar(*(str + i));
+		i++;
+	}
+	putchar(10);
 }
