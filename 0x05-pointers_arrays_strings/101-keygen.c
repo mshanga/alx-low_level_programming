1b9171db2fe2ce397af651771a5c364d2913021d
“Emmanuel <emakonde@gmail.com>
2022-09-20 01:25:42 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,24 +1,27 @@
-include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "main.h"
+
 /**
-* main - generates keygen.
-* Return: 0 Always.
-*/
+ * main - entry point
+ *
+ * Return: generated password
+ */
+
 int main(void)
 {
-int r = 0, c = 0;
-time_t t;
+	char c;
+	int x;
 
-srand((unsigned int) time(&t));
-while (c < 2772)
-{
-r = rand() % 128;
-if ((c + r) > 2772)
-break;
-c = c + r;
-printf("%c", r);
-}
-printf("%c\n", (2772 - c));
-return (0);
+	srand(time(0));
+	while (x <= 2645)
+	{
+		c = rand() % 128;
+		x += c;
+		putchar(c);
+	}
+	putchar(2772 - x);
+
+	return (0);
 }
